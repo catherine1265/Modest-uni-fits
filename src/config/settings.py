9e86e07f6a399722f6e0599ae6b73f13ae5e3496
catherine1265 <catherine.sharonn@gmail.com>
2026-06-04 15:12:58 +0700

    Create settings.py

---
+++ src/config/settings.py
@@ -0,0 +1,10 @@
+TOP_MODEL_PATH    = "src/models/top_model.pkl"
+BOTTOM_MODEL_PATH = "src/models/bottom_model.pkl"
+CARD_MODEL_PATH   = "src/models/card_model.joblib"
+
+IMG_SIZE = (128, 128)
+
+CONF_THRESH = 0.60
+
+TOP_CLASSES    = ["dress", "longsleeve", "outwear", "shirt", "t-shirt"]
+BOTTOM_CLASSES = ["pants", "shorts", "skirt"]
